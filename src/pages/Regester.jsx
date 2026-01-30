0af0391132c31b18911732b6c05001641485835b
Anil Parmar <your-email-linked-to-akparmar0699@gmail.com>
2026-01-30 14:58:56 +0530

    register file upload

---
+++ src/pages/Regester.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Regester = () => {
+  return (
+    <>
+    <div className="form-container">
+       <h1>This Is Register.</h1>
+    </div>
+    </>
+  )
+}
+
+export default Regester
